15b2f6b30937be328440db7ed9ab8dcabe6b9200
crcali <33082459+crcali@users.noreply.github.com>
2019-10-20 17:06:33 -0500

    finalized arduino code
    Utlizes:
    - stepper motor for stirring
    - pumps and pressure sensor for maintenance of pressure
    - temperature and heater for maintenance of temperature
    - button to switch between modes

--- bioreactor_main/bioreactor_main.ino
+++ bioreactor_main/bioreactor_main.ino
@@ -1,17 +1,113 @@
-#include <dht.h>
+// AUTHOR: CCA iGEM 
+// BUILD ON ARDUINO GENUINO UNO
 
-dht DHT;
+#include "dht.h"
+#include "programmable_air.h"
+#include <Stepper.h>
+int stepCount = 0;  // number of steps the motor has taken
 
+// temperature instantiation  
+dht DHT;     // variable for the DHT temperature sensor
+const int buttonPin = 2;     // the number of the pushbutton pin
 #define DHT11_PIN 7
 
-void setup() {
-  // put your setup code here, to run once:
+// heater instantiation
+#define PIN_SWITCH 5
 
+// button instantiation
+int buttonState = 0;         // variable for reading the pushbutton status
+String mode = "O2";         // mode of bioreactor; default is O2
+
+// pressure instantiation
+float setPressure; 
+float tolerence;
+int state;
+
+// stepper motor instantiation for stirrer
+const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+
+// potentiometer setup
+
+void setup() {
+  pinMode(buttonPin, INPUT); // initialize the pushbutton pin as an input
+  initializePins(); // initilize pins for pressure maintenance using pumps
+  pinMode(PIN_SWITCH, OUTPUT); // initialize the heater's pin
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int chk = DHT.read11(DHT11_PIN);
-  delay(1000);
+  int chk = DHT.read11(DHT11_PIN);     // checks DHT sensor
+  buttonState = digitalRead(buttonPin);
+  float pressure = readPressure();
+  if (buttonState == LOW) {
+    if (mode == "O2") {
+      mode = "N2"; // if mode is O2, set to N2 if button pressed
+    }
+    if (mode == "N2") {
+      mode == "O2"; // if mode is N2, set to O2 if button pressed
+    }
+  }
+  
+  if (mode == "N2") {
+    maintainPressure(pressure,2,.25,1); // maintain a pressure of 2 atm on pump 2 with a tolerance of 0.25 atm
+    maintainTemperature(DHT.temperature, 22, 1); // maintain a temperature of 
 
+    int sensorReading = analogRead(A0); // read the potentiometer's value
+    int motorSpeed = map(sensorReading, 0, 1023, 0, 100); // map it to a range from 0 to 100:
+
+    myStepper.setSpeed(motorSpeed); // set the stepper motor's speed
+    myStepper.step(stepsPerRevolution / 100); // step 1/100 of a revolution
+  }
+
+  if (mode == "O2") {
+    maintainPressure(pressure,2,.25,2); // maintain a pressure of 2 atm on pump 2 with a tolerance of 0.25 atm
+    maintainTemperature(DHT.temperature, 22, 1); // maintain a temperature of 
+
+    int sensorReading = analogRead(A0); // read the potentiometer's value
+    int motorSpeed = map(sensorReading, 0, 1023, 0, 100); // map it to a range from 0 to 100:
+
+    myStepper.setSpeed(motorSpeed); // set the stepper motor's speed
+    myStepper.step(stepsPerRevolution / 100); // step 1/100 of a revolution
+  }
+
+  delay(100);
+
+}
+
+void maintainTemperature(int temperature, int setTemperature, int tolerance) {
+  if (temperature > setTemperature + tolerence) {
+    digitalWrite(PIN_SWITCH, LOW); //  heater off by setting power output to low
+  }
+  if (temperature < (setTemperature - tolerence)) {
+    digitalWrite(PIN_SWITCH, HIGH); // heater on by setting power output to high
+  }
+}
+ 
+void maintainPressure (int pressure, int setTemperature, int tolerance, int pump) {
+  if (pressure > setPressure + tolerence) {
+    vent(pump); // vent the pump
+    state = DECREASING; // set the state to decreasing
+    //    suck(pump);
+  }
+  else {
+    if (pressure < (setPressure - tolerence)) {
+      blow(pump); // turn on the pump
+      state = INCREASING; // set the state to incresing
+    }
+    else {
+      if (state == UN_KNOWN)
+      {
+        closeAll(pump); // close the pump due to malfunction
+      }
+    }
+  }
+  
+  if (pressure < setPressure && state == DECREASING) {
+  state = UN_KNOWN;
+  }
+  
+  if (pressure > setPressure && state == INCREASING) {
+  state = UN_KNOWN;
+  }
 }
---
+++ bioreactor_main/dht.cpp
@@ -0,0 +1,154 @@
+//
+//    FILE: dht.cpp
+// PURPOSE: DHT Temperature & Humidity Sensor library for Arduino
+//
+
+
+#include "dht.h"
+
+/////////////////////////////////////////////////////
+//
+// PUBLIC
+//
+
+// return values:
+// DHTLIB_OK
+// DHTLIB_ERROR_CHECKSUM
+// DHTLIB_ERROR_TIMEOUT
+int dht::read11(uint8_t pin)
+{
+    // READ VALUES
+    int rv = _readSensor(pin, DHTLIB_DHT11_WAKEUP);
+    if (rv != DHTLIB_OK)
+    {
+        humidity    = DHTLIB_INVALID_VALUE; // invalid value, or is NaN prefered?
+        temperature = DHTLIB_INVALID_VALUE; // invalid value
+        return rv;
+    }
+
+    // CONVERT AND STORE
+    humidity    = bits[0];  // bits[1] == 0;
+    temperature = bits[2];  // bits[3] == 0;
+
+    // TEST CHECKSUM
+    // bits[1] && bits[3] both 0
+    uint8_t sum = bits[0] + bits[2];
+    if (bits[4] != sum) return DHTLIB_ERROR_CHECKSUM;
+
+    return DHTLIB_OK;
+}
+
+
+// return values:
+// DHTLIB_OK
+// DHTLIB_ERROR_CHECKSUM
+// DHTLIB_ERROR_TIMEOUT
+int dht::read(uint8_t pin)
+{
+    // READ VALUES
+    int rv = _readSensor(pin, DHTLIB_DHT_WAKEUP);
+    if (rv != DHTLIB_OK)
+    {
+        humidity    = DHTLIB_INVALID_VALUE;  // invalid value, or is NaN prefered?
+        temperature = DHTLIB_INVALID_VALUE;  // invalid value
+        return rv; // propagate error value
+    }
+
+    // CONVERT AND STORE
+    humidity = word(bits[0], bits[1]) * 0.1;
+    temperature = word(bits[2] & 0x7F, bits[3]) * 0.1;
+    if (bits[2] & 0x80)  // negative temperature
+    {
+        temperature = -temperature;
+    }
+
+    // TEST CHECKSUM
+    uint8_t sum = bits[0] + bits[1] + bits[2] + bits[3];
+    if (bits[4] != sum)
+    {
+        return DHTLIB_ERROR_CHECKSUM;
+    }
+    return DHTLIB_OK;
+}
+
+/////////////////////////////////////////////////////
+//
+// PRIVATE
+//
+
+// return values:
+// DHTLIB_OK
+// DHTLIB_ERROR_TIMEOUT
+int dht::_readSensor(uint8_t pin, uint8_t wakeupDelay)
+{
+    // INIT BUFFERVAR TO RECEIVE DATA
+    uint8_t mask = 128;
+    uint8_t idx = 0;
+
+    // replace digitalRead() with Direct Port Reads.
+    // reduces footprint ~100 bytes => portability issue?
+    // direct port read is about 3x faster
+	uint8_t bit = digitalPinToBitMask(pin);
+	uint8_t port = digitalPinToPort(pin);
+    volatile uint8_t *PIR = portInputRegister(port);
+
+    // EMPTY BUFFER
+    for (uint8_t i = 0; i < 5; i++) bits[i] = 0;
+
+    // REQUEST SAMPLE
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, LOW); // T-be 
+    delay(wakeupDelay);
+    digitalWrite(pin, HIGH);   // T-go
+    delayMicroseconds(40);
+    pinMode(pin, INPUT);
+
+    // GET ACKNOWLEDGE or TIMEOUT
+    uint16_t loopCntLOW = DHTLIB_TIMEOUT;
+    while ((*PIR & bit) == LOW )  // T-rel
+    {
+        if (--loopCntLOW == 0) return DHTLIB_ERROR_TIMEOUT;
+    }
+
+    uint16_t loopCntHIGH = DHTLIB_TIMEOUT;
+    while ((*PIR & bit) != LOW )  // T-reh
+    {
+        if (--loopCntHIGH == 0) return DHTLIB_ERROR_TIMEOUT;
+    }
+
+    // READ THE OUTPUT - 40 BITS => 5 BYTES
+    for (uint8_t i = 40; i != 0; i--)
+    {
+        loopCntLOW = DHTLIB_TIMEOUT;
+        while ((*PIR & bit) == LOW )
+        {
+            if (--loopCntLOW == 0) return DHTLIB_ERROR_TIMEOUT;
+        }
+
+        uint32_t t = micros();
+
+        loopCntHIGH = DHTLIB_TIMEOUT;
+        while ((*PIR & bit) != LOW )
+        {
+            if (--loopCntHIGH == 0) return DHTLIB_ERROR_TIMEOUT;
+        }
+
+        if ((micros() - t) > 40)
+        { 
+            bits[idx] |= mask;
+        }
+        mask >>= 1;
+        if (mask == 0)   // next byte?
+        {
+            mask = 128;
+            idx++;
+        }
+    }
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, HIGH);
+
+    return DHTLIB_OK;
+}
+//
+// END OF FILE
+//
